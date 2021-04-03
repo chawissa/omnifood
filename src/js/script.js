cafab37d0e1324c9fed2f571eba61010cd74eb5c
Charissa Ramirez <chawissa@gmail.com>
2021-04-03 12:01:39 -0400

    Nav scroll

--- src/js/script.js
+++ src/js/script.js
@@ -26,4 +26,22 @@ $(document).ready(function () {
       1000
     );
   });
+
+  /* ------------ NAVIGATION SCROLL ------------ */
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
 });
