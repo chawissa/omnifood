08293a55805e447e0d4a79b6f0b50160c4c2f0e1
Charissa Ramirez <chawissa@gmail.com>
2021-04-03 12:45:53 -0400

    Animation scroll

--- src/js/script.js
+++ src/js/script.js
@@ -37,11 +37,48 @@ $(document).ready(function () {
         {
           scrollTop: $(hash).offset().top,
         },
-        800,
+        1000,
         function () {
           window.location.hash = hash;
         }
       );
     }
   });
+
+  /* ------------ ANIMATIONS ON SCROLL ------------ */
+  $(".js--wp-1").waypoint(
+    function (direction) {
+      $(".js--wp-1").addClass("animate__animated animate__fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-2").waypoint(
+    function (direction) {
+      $(".js--wp-2").addClass("animate__animated animate__fadeInUp");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-3").waypoint(
+    function (direction) {
+      $(".js--wp-3").addClass("animate__animated animate__fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-4").waypoint(
+    function (direction) {
+      $(".js--wp-4").addClass("animate__animated animate__pulse");
+    },
+    {
+      offset: "50%",
+    }
+  );
 });
