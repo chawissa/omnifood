ccc9317ada848ebd1c5b3ec98c788bb777dc28c2
Charissa Ramirez <chawissa@gmail.com>
2021-04-04 11:29:19 -0400

    Responsive nav

--- src/js/script.js
+++ src/js/script.js
@@ -81,4 +81,24 @@ $(document).ready(function () {
       offset: "50%",
     }
   );
+
+  $(window).resize(function () {
+    var nav = $(".js--main-nav");
+
+    var icon = $(".js--nav-icon i");
+
+    if ($(window).width() > 767) {
+      nav.css("display", "block");
+
+      icon.addClass("ion-close-round");
+
+      icon.removeClass("ion-navicon-round");
+    } else {
+      nav.css("display", "none");
+
+      icon.addClass("ion-navicon-round");
+
+      icon.removeClass("ion-close-round");
+    }
+  });
 });
